91bc394c1d972669efef579774e7930012cdf83f
planetjunkie <dayem.uae@proton.me>
2022-11-05 19:51:42 +0400

    major changes to stuff lol

---
+++ lib/accscreens/loginscreen.dart
@@ -0,0 +1,74 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:SpawnPoint/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+void main(List<String> args) {
+  runApp(const loginscreen());
+}
+
+class loginscreen extends StatelessWidget {
+  const loginscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveTheme(
+        light: ThemeData(
+          fontFamily: 'RedHatDisplay',
+          useMaterial3: true,
+          brightness: Brightness.light,
+          primarySwatch: Colors.purple,
+          primaryColor: Colors.white,
+        ),
+        dark: ThemeData(
+          fontFamily: 'RedHatDisplay',
+          useMaterial3: true,
+          brightness: Brightness.dark,
+          primarySwatch: Colors.purple,
+          primaryColor: Colors.white,
+          textTheme: TextTheme(
+            
+          )
+        ),
+        initial: AdaptiveThemeMode.system,
+        builder: ((light, dark) => MaterialApp(
+              home: Scaffold(
+                  appBar: AppBar(
+                    title: const Text("Login to SpawnPoint"),
+                    leading: BackButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const app()));
+                      },
+                    ),
+                  ),
+                  body: Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Center(
+                        child: Column(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        const TextField(
+                          obscureText: false,
+                          decoration: InputDecoration(
+                              icon: Icon(FeatherIcons.user),
+                              border: OutlineInputBorder(),
+                              labelText: "Email/Username"),
+                        ),
+                        const TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              icon: Icon(FeatherIcons.lock),
+                              border: OutlineInputBorder(),
+                              labelText: "Password"),
+                        ),
+                      ],
+                    )),
+                  )),
+              theme: light,
+              darkTheme: dark,
+            )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import "accscreens/loginscreen.dart";
+import "accscreens/signupscreen.dart";
+import 'package:adaptive_theme/adaptive_theme.dart';
 
 void main() {
   runApp(app());
@@ -6,63 +9,93 @@ void main() {
 
 class app extends StatelessWidget {
   const app({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.highContrastDark(),
-        useMaterial3: true,
-      ),
-      title: "SpawnPoint",
-      home: Scaffold(
-        appBar: AppBar(title: const Text("SpawnPoint")),
-        body: const mainlayout(),
-      ),
-    );
+    return AdaptiveTheme(
+        light: ThemeData(
+          fontFamily: 'RedHatDisplay',
+          useMaterial3: true,
+          brightness: Brightness.light,
+          primarySwatch: Colors.purple,
+          primaryColor: Color.fromARGB(255, 233, 30, 182),
+        ),
+        dark: ThemeData(
+          fontFamily: 'RedHatDisplay',
+          useMaterial3: true,
+          brightness: Brightness.dark,
+          primarySwatch: Colors.purple,
+          primaryColor: Color.fromARGB(255, 233, 30, 182),
+        ),
+        initial: AdaptiveThemeMode.system,
+        builder: ((light, dark) => MaterialApp(
+              title: "SpawnPoint",
+              home: Scaffold(
+                appBar: AppBar(title: const Text("")),
+                body: const mainlayout(),
+              ),
+              theme: light,
+              darkTheme: dark,
+            )));
   }
 }
 
-class mainlayout extends StatelessWidget {
+class mainlayout extends StatefulWidget {
   const mainlayout({super.key});
 
   @override
+  State<mainlayout> createState() => _mainlayoutState();
+}
+
+class _mainlayoutState extends State<mainlayout> {
+  @override
   Widget build(BuildContext context) {
     return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Card(child: Padding(
-          padding: EdgeInsets.all(15.0),
-          child: Column(
-          children: <Widget>[
-            const ListTile(
-              leading: Icon(Icons.code),
-              title: Text("Welcome to SpawnPoint"),
-              subtitle: Text("This project is being ctively Worked on (See Button Below)"),
-
-            )
+          children: [
+            
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    child: const Text(
+                      "Create an account",
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const signupscreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 233, 30, 182)
+                    ),
+              ),
+                  ElevatedButton(
+                    child: const Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const loginscreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 233, 30, 182)
+                    ),
+                    
+                    
+                )
+                ],
+              )
+            ),
+            
           ],
         ),
-        )
-        ),
-        Card(
-            child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            // ignore: prefer_const_literals_to_create_immutables
-            children: <Widget>[
-              const ListTile(
-                leading: Icon(Icons.warning),
-                title: Text("SpawnPoint is being Constructed"),
-                subtitle: Text(
-                    "The source code is being Rewritten in flutter, This is NOT a Working project"),
-              ),
-            ],
-          ),
-        ))
-      ],
-    ));
+      ),
+    );
   }
 }
